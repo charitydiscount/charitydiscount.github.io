126ed3046a916750fddc766fc94a2b496b18b350
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-18 06:15:55 +0000

    Deploy charitydiscount/charitydiscount.github.io to github.com/charitydiscount/charitydiscount.github.io.git:master

---
+++ precache-manifest.b83706dbaa3c958282d6eef766e92f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dc0ffc19172e43470e3ec6e2e067f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba8e86b1ed26c8315d41",
+    "url": "/static/css/main.abaea082.chunk.css"
+  },
+  {
+    "revision": "613b487f7486e3b8ca12",
+    "url": "/static/js/2.e9e1fb83.chunk.js"
+  },
+  {
+    "revision": "e39c918a8bb7eb869204bc46d69e20cd",
+    "url": "/static/js/2.e9e1fb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8e86b1ed26c8315d41",
+    "url": "/static/js/main.f15c5622.chunk.js"
+  },
+  {
+    "revision": "1e349c64efdc1d775629",
+    "url": "/static/js/runtime-main.4fbcdb17.js"
+  }
+]);
